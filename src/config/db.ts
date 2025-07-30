487f1aaaa24eba4e480365466171b7630363fc6c
KimV <71965590+dezineskv@users.noreply.github.com>
2025-07-29 21:45:22 -0700

    cleanup of files

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,8 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const MONGO_URI = "mongodb://localhost:27017/bikeStoreDB"; // Or your remote URI
+const MONGO_URI = "mongodb://localhost:27017/bikeStoreDB"; // Or your remote URI
 
-// mongoose
-//   .connect(MONGO_URI)
-//   .then(() => console.log("✅ MongoDB connected"))
-//   .catch((err) => console.error("❌ MongoDB connection error:", err));
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
