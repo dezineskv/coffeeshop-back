b9281e7c13827af96341a4a9b5051879d389df57
KimV <71965590+dezineskv@users.noreply.github.com>
2025-07-30 00:32:32 -0700

    typescript omission

--- src/config/db.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const MONGO_URI = "mongodb://localhost:27017/bikeStoreDB"; // Or your remote URI
-
-mongoose
-  .connect(MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
