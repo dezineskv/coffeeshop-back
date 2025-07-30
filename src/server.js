176981e5a762e77e1650c629234b8a8fc8d53d82
KimV <71965590+dezineskv@users.noreply.github.com>
2025-07-30 00:28:34 -0700

    package update

--- src/server.js
+++ src/server.js
@@ -70,6 +70,7 @@ app.get("/", (req, res) => {
 //   if (!product) return res.status(404).send({ error: "Product not found" });
 // });
 // });
+
 // Start the server
 // app.listen(PORT, () => {
 //   console.log(`Server is running on http://localhost:${PORT}`);
